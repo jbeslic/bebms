420338a9ac16fb239dbfe2533fa9e2aee3e75083
jbeslic <josipa.beslic@gmail.com>
2020-05-08 14:29:34 +0200

    Create 2020_05_08_115135_create_expenses_table.php

---
+++ database/migrations/2020_05_08_115135_create_expenses_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExpensesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('associates', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+        Schema::create('projects', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('project_id');
+            $table->integer('associate_id')->nullable();
+            $table->text('description');
+            $table->integer('unit_id');
+            $table->float('amount');
+            $table->float('price');
+            $table->integer('discount')->nullable();
+            $table->string('currency')->default('HRK');
+            $table->string('hnb_middle_exchange')->nullable();
+            $table->date('expense_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expenses');
+        Schema::dropIfExists('associates');
+        Schema::dropIfExists('projects');
+    }
+}
